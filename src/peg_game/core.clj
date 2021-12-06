bec5a026aeff87f56d9efcf2f580d1db42917987
James Covert <jcovert2@asu.edu>
2021-12-06 00:09:20 -0800

    added triangular functions as well as connect

--- src/peg_game/core.clj
+++ src/peg_game/core.clj
@@ -1,5 +1,5 @@
 (ns peg-game.core
-  (require [clojure.set :as set])
+  (:require [clojure.set :as set])
   (:gen-class))
 
 (declare successful-move prompt-move game-over query-rows)
@@ -9,7 +9,8 @@
   [& args]
   (println "Welcome to the Peg Game!!"))
 
-(def board 
+(def board
+  "board that game is played on"
   {1  {:pegged true, :connections {6 3, 4 2}},
    2  {:pegged true, :connections {9 5, 7 4}},
    3  {:pegged true, :connections {10 6, 8 5}},
@@ -26,3 +27,39 @@
    14 {:pegged true, :connections {12 13, 5 9}},
    15 {:pegged true, :connections {13 14, 6 10}},
    :rows 5})
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri
+  "Calls tri* and binds it to tri"
+  (tri*))
+
+(defn triangular?
+  "Is the number triangular?"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of the row"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns row number the position belongs to"
+  [position]
+  (inc (count (take-while #(> position %) tri))))
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos position neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+            [[position destination] [destination position]])
+    board))
